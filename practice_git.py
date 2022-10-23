b90e007ce1731d82feca28d2b3d10cc74fc32f92
RSylla <103101146+RSylla@users.noreply.github.com>
2022-10-23 14:38:30 +0300

    Lisasin mõistatuse.
    Kes ära arvab, lisab uue mõistatuse.

---
+++ practice_git.py
@@ -0,0 +1,19 @@
+"""
+See fail on Git protsesside harjutamiseks.
+Siia kirjutame iga päev igaüks ühe rea.
+
+Tingimused:
+* Muudatuste üles laadimiseks vaja kõigi tiimiliikmete kinnitust!
+* Main branchi keegi ei merge ja DEV branchi jaoks teevad kõik liikmed oma branchi.
+* Kommenteerime oma muudatusi, et kõik teaksid, mida on tehtud.
+
+NB! Muudatuste üles laadimisel tekkivad küsimused lahkame jooksvalt
+    slackis FinalProject juturuumis, sellisel moel saame kõik osa
+    probleemist ja selle lahendusest.
+
+NB! soovitatav on ennem muudatuste tegemist "merge"-da div branch enda oma külge.
+    Sel moel on lihtsam vältida konflikti, et oma muudatuste üles laadmise ajaks
+    on div-i muudetud.
+"""
+
+riddle = """Mõista mõista, mis see on: Istub kui isand, tolmab kui tont?"""
