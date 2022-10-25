b0b2f5133d50937230b96b4a6c6fda2d3e7cdab5
kl0022 <110675200+kl0022@users.noreply.github.com>
2022-10-25 14:29:22 +0300

    Vastasin ja lisasin uue mõistatuse

--- practice_git.py
+++ practice_git.py
@@ -23,3 +23,5 @@ riddle = """Mõista mõista, mis see on: Istub kui isand, tolmab kui tont?"""
 vastus = "Korsten"
 
 riddle2 = "Tänapäevasem mõistatus an4-lt: Mees käib ööd, käib päevad, ei väsi ilmaski"
+solution = Väsimatu mees :)
+riddle_3 = Kanda jõuad, lugeda ei jõua ?
