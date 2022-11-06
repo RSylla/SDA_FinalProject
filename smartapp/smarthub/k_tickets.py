425256e1d8647cb697c5726ec7cbaf18940a78f4
RSylla <103101146+RSylla@users.noreply.github.com>
2022-11-06 11:08:44 +0200

    Edited tickets 1.06, 1.08, 1.12
    Commited by Raivo

--- smartapp/smarthub/k_tickets.py
+++ smartapp/smarthub/k_tickets.py
@@ -1,56 +1,30 @@
-# 1.06 Add logic for removing punctuations
-
 import wikipedia
-import string
 
 
 # 1.06 Add logic for removing punctuations
-
-text = input("Enter your text: ")
 def punctuation_removal(text):
     punctuation = ''''!()-[]{};:'"\,<>./?@#$%^&*_~¤'''
-
     for char in text:
         if char in punctuation:
             text = text.replace(char, "")
     return text
 
 
-
-
 #1.07 Add logic to convert text to upper case and lower case
-
-toggle_upper = True
-toggle_lower = False
-
-text = input("Enter your text: ")
 def text_upper(text):
     return text.upper()
+
 
 def text_lower(text):
     return text.lower()
 
 
-
 # 1.08 Add logic to remove new line from text
-
-text = "\nThis is a test case.\nType in your text."
 def remove_new_line(text):
     return " ".join(text.split())
 
 
-
-
-
 #1.12 Add logic to generate summary of word
-example = "amazon"
 def word_summary(example):
     summary = wikipedia.summary(example)
     return summary
-
-
-print(word_summary(example))
-
-
-
-
