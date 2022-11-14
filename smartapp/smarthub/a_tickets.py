f53734199bed27a50d8117aa7365045275b1ab66
An4Sil <anneli.silbaum@gmail.com>
2022-11-14 14:08:22 +0100

    An4 finished tickets 1.09, 1.10, 1.11, 1.13+tests

--- smartapp/smarthub/a_tickets.py
+++ smartapp/smarthub/a_tickets.py
@@ -2,7 +2,7 @@ from textblob import TextBlob, Word
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
-from smartapp.smarthub.k_tickets import punctuation_removal
+from .k_tickets import punctuation_removal
 
 # 1.09 Add logic to remove extra space
 def remove_extra(string):
@@ -14,33 +14,38 @@ def remove_extra(string):
 
 
 # 1.10 Add logic to count characters
-def count_characters(string):
+def count_characters(text):
 
-    if string:
-        with_spaces = len(string)
-        without_spaces = len(string) - string.count(' ')
-        spaces = string.count(' ')
-        num_words = len(string.split())
+    if text:
+        with_spaces = len(text)
+        without_spaces = len(text) - text.count(' ')
+        spaces = text.count(' ')
+        num_words = int(len(text.split()))
 
-    return (f"All characters: {with_spaces} \nOnly characters (without spaces): {without_spaces} \nSpaces: {spaces} \nWords: {num_words}")
+        char_count = f"All characters: {with_spaces};"
+        char_wo_spaces = f"Only characters without spaces: {without_spaces};"
+        space_count = f"Spaces: {spaces};"
+        word_count = f" Words: {num_words};"
+
+        return char_count, char_wo_spaces, space_count, word_count
 
 
 #1.11 Add logic to check for text spellings
-
 def spell_check(text):
-    text = punctuation_removal(text)
     corrected = TextBlob(text)
     incorrect_words = []
     for word in text.split():
         wordy = Word(word).spellcheck()[0]
         if wordy[0] != word and wordy[1] >= 0.7:
             incorrect_words.append(word)
-    print(incorrect_words)
-    return (f"Corrected text: {corrected.correct()}, \nThese words were incorrect: {incorrect_words}")
+
+    corrected_text = corrected.correct()
+    incorrect_words= f"Incorrect words: {', '.join(incorrect_words)}!"
+
+    return corrected_text, incorrect_words
 
 
 # 1.13 Add logic to remove stop words from text
-
 def remove_stopwords(text):
     text = punctuation_removal(text)
     stop_words = set(stopwords.words('english'))
@@ -52,7 +57,11 @@ def remove_stopwords(text):
         if w not in stop_words:
 
             filtered_sentence.append(w)
-    return (f"Original sentence: {text}, \nFiltered sentence without stopwords: {filtered_sentence}")
+
+    wo_stopwords = f"{', '.join(filtered_sentence)}"
+
+    return wo_stopwords
+
 
 
 
--- smartapp/smarthub/tests.py
+++ smartapp/smarthub/tests.py
@@ -1,39 +1,25 @@
 import unittest
-from smartapp.smarthub.k_tickets import *
-from smartapp.smarthub.a_tickets import *
+from .a_tickets import *
 
 class FunctionalityTest(unittest.TestCase):
 
-    def test_remove_punctuations(self):
-        pass
-
-    def test_upper_case(self):
-        pass
-
-    def test_lower_case(self):
-        pass
-
-    def test_new_line_remove(self):
-        pass
-
     def test_when_extra_space_removed_then_true (self):
-        self.assertTrue(remove_extra("") == "")
-        self.assertFalse(remove_extra("") == "")
+        self.assertTrue(remove_extra("  This  is  with   spaces!  ") == "This is with spaces!")
+        self.assertFalse(remove_extra("  This  is  with   spaces!  ") == "  This  is  with   spaces!  ")
 
     def test_when_characters_counted_then_true(self):
-        self.assertTrue(count_characters("with spaces!") == "with spaces!")
-        self.assertFalse(count_characters("with spaces!") == "with spaces!")
+        self.assertTrue(count_characters("counting characters!") == ('All characters: 20;', 'Only characters without spaces: 19;', 'Spaces: 1;', ' Words: 2;'))
+        self.assertFalse(count_characters("counting characters!") == ('All characters: 2;', 'Only characters without spaces: 1;', 'Spaces: 1;', ' Words: 2;'))
 
 
-    def test_spell_check(self):
-        self.assertTrue(spell_check("is this sentence correct") == "is this sentence correct")
-        self.assertFalse(spell_check("is this sentence correct") == "is this sentence correct")
+    def test_when_spell_check_corrected_the_true(self):
+        self.assertTrue(spell_check("It is verry loovely dday.") == "It is very lovely day.")
+        self.assertFalse(spell_check("It is verry loovely dday.") == "It is verry loovely dday.")
 
-    def test_gen_word_summary(self):
-        pass
 
-    def test_remove_stop(self):
-        pass
+    def test_when_stop_words_removed_then_true(self):
+        self.assertTrue(remove_stopwords("This is a random sentence") == "random, sentence")
+        self.assertFalse(remove_stopwords("This is a random sentence") == "This is a random sentence")
 
 if __name__ == '__main__':
     unittest.main()
