2c345bb1ffccae3b2f61e3efd3e455454ac011fb
kl0022 <kristinalytovchenko@gmail.com>
2022-11-19 14:07:34 +0200

    Updated files

--- smartapp/smarthub/a_tickets.py
+++ smartapp/smarthub/a_tickets.py
@@ -1,15 +1,15 @@
-from textblob import TextBlob, Word
+from string import punctuation
 import nltk
+nltk.download()
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+from textblob import TextBlob, Word
 from .k_tickets import punctuation_removal
-from string import punctuation
 
 
 # 1.09 Add logic to remove extra space
 
 def remove_extra(string):
-
     while '  ' in string:
         string = string.replace('  ', ' ')
 
@@ -18,7 +18,6 @@ def remove_extra(string):
 
 # 1.10 Add logic to count characters
 def count_characters(text):
-
     if text:
         with_spaces = len(text)
         without_spaces = len(text) - text.count(' ')
@@ -33,21 +32,21 @@ def count_characters(text):
         return char_count, char_wo_spaces, space_count, word_count
 
 
-#1.11 Add logic to check for text spellings
+# 1.11 Add logic to check for text spellings
 def spell_check(text):
     corrected = TextBlob(text)
     incorrect_words = []
     for word in text.split():  # makes a list and checks for each word
         word = punctuation_removal(word, punctuation)
         wordy = Word(word).spellcheck()[0]  # returns a tuple of suggested word and probability
-        if wordy[0] != word and wordy[1] >= 0.7: #it compares the word entered by user if the probability that the word we want is correct over 70%
+        if wordy[0] != word and wordy[
+            1] >= 0.7:  # it compares the word entered by user if the probability that the word we want is correct over 70%
             incorrect_words.append(word)
 
     corrected_text = corrected.correct()
-    incorrect_words= f"Incorrect words: {', '.join(incorrect_words)}!"
+    incorrect_words = f"Incorrect words: {', '.join(incorrect_words)}!"
 
     return corrected_text, incorrect_words
-
 
 
 # 1.13 Add logic to remove stop words from text
@@ -60,15 +59,8 @@ def remove_stopwords(text):
 
     for w in word_tokens:
         if w not in stop_words:
-
             filtered_sentence.append(w)
 
     wo_stopwords = f"{', '.join(filtered_sentence)}"
 
     return wo_stopwords
-
-
-
-
-
-
